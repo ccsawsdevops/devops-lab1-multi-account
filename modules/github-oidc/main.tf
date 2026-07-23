f488361b6c13494a18b63d95657191be68a3ab7c
Charles <ccsawsdevops@gmail.com>
2026-07-23 17:33:15 -0600

    fix: hardcode repo in OIDC trust policy

--- modules/github-oidc/main.tf
+++ modules/github-oidc/main.tf
@@ -1,13 +1,11 @@
 data "aws_caller_identity" "current" {}
 
-# Register GitHub as an OIDC Identity Provider in AWS
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
 }
 
-# IAM Role assumed by GitHub Actions
 resource "aws_iam_role" "github_actions" {
   name = "github-actions-terraform-role"
 
@@ -25,9 +23,7 @@ resource "aws_iam_role" "github_actions" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            # Notice "repo:${var.github_org_or_user}/${var.github_repo}:*"
-            # Matches claims like "repo:ccsawsdevops/devops-lab1-multi-account:ref:refs/heads/main"
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_org_or_user}/${var.github_repo}:*"
+            "token.actions.githubusercontent.com:sub" = "repo:ccsawsdevops/devops-lab1-multi-account:*"
           }
         }
       }
@@ -35,7 +31,6 @@ resource "aws_iam_role" "github_actions" {
   })
 }
 
-# Attach AdministratorAccess for Terraform deployments
 resource "aws_iam_role_policy_attachment" "github_actions_admin" {
   role       = aws_iam_role.github_actions.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
